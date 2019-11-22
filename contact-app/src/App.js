f5a15651d9557d778bd0c1020d5293d4abf48e88
myeonghuSong <21300390@handong.edu>
2019-11-23 02:39:20 +0900

    배열 렌더링하기

--- contact-app/src/App.js
+++ contact-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList';
 
 class App extends Component {
 
@@ -23,7 +24,7 @@ class App extends Component {
     return (
       <div>
         <PhoneForm onCreate={this.handlecreate} />
-        {JSON.stringify(this.state.information)}
+        <PhoneInfoList data={this.state.information } />
       </div>
     );
   }
